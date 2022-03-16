d713b5e2d524b0aabd7dc0dad6974a85644fd60f
mohdosaid4 <mohdosaid4@gmail.com>
2022-03-16 16:30:12 +0530

    JWT

---
+++ src/main/java/com/jwtAuth/controllers/Home.java
@@ -0,0 +1,12 @@
+package com.jwtAuth.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class Home {
+
+
+}
